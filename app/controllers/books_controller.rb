60084d174c01f7128333f2ff7278c3a7329ed9ec
rahmatyana26 <90589127+rahmatyana26@users.noreply.github.com>
2023-02-16 15:18:11 +0700

    POST END POINT

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,16 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    book = Book.new(author: 'Raditya Dika', title: 'Kambing Jantan')
+
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unproccessable_entity
+    end
+    
+  end
+
 end
