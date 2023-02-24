258d8a47c3dafab0557320cba922851cc979dd7c
rahmatyana26 <90589127+rahmatyana26@users.noreply.github.com>
2023-02-25 02:52:40 +0700

    Controller Representers

--- app/representers/books_representer.rb
+++ app/representers/books_representer.rb
@@ -1,20 +1,25 @@
 class BooksRepresenter
     def initialize (books)
-        @book = books
+        @books = books
     end
 
     def as_json
         books.map do |book|
             {
                 id: book.id,
-                title: book.title,
-                author_firts_name: book.author.firts_name,
-                author_last_name: book.author.last_name,
+                title: book.title, 
+                author_name: author_name(book),
                 author_age: book.author.age
             }
+        end  
     end
 
     private
+
+    def author_name(book)
+        "#{book.author.first_name} #{book.author.last_name}"
+        
+    end
 
     attr_reader :books
 end
