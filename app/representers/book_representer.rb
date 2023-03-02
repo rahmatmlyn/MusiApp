46b9fe96fd41adeca5d08ada4b109f9a5c179382
rahmatyana26 <90589127+rahmatyana26@users.noreply.github.com>
2023-03-02 14:19:19 +0700

    API Response Body Specs & Using Spec Helpers

--- app/representers/book_representer.rb
+++ app/representers/book_representer.rb
@@ -1,25 +1,24 @@
-class BooksRepresenter
-    def initialize (books)
-        @books = books
+class BookRepresenter
+    def initialize (book)
+        @book = book
     end
 
     def as_json
-        books.map do |book|
             {
                 id: book.id,
                 title: book.title, 
                 author_name: author_name(book),
                 author_age: book.author.age
             }
-        end  
     end
 
     private
 
-    attr_reader :books
+    attr_reader :book
 
     def author_name(book)
         "#{book.author.first_name} #{book.author.last_name}"
     end
 
+    
 end
